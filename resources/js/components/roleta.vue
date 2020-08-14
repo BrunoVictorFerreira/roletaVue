f2e1ee4b732618a6f81965f585bc48ef8cbeecb0
Bruno Victor <victorbruno221@gmail.com>
2020-08-14 16:15:43 -0300

    Add Funcionality

--- resources/js/components/roleta.vue
+++ resources/js/components/roleta.vue
@@ -1,11 +1,5 @@
 <template>
-<img src="../../../public/storage/seletor.svg" style="position:absolute;top: 0;left: 300px;z-index: 100">
-    <!-- <img src="../../../public/storage/roleta.svg" id="roleta"> -->
+    <img src="../../../public/storage/roleta.svg">
     
 
 </template>
-<script>
-    setTimeout(function(){
-        document.getElementById("roleta").style.animationIterationCount = "inherit";
-    },2000);
-</script>
--- resources/views/roleta.blade.php
+++ resources/views/roleta.blade.php
@@ -2,18 +2,38 @@
 <head>
 <style>
     #roleta{
-        animation-name: roleta;
-        animation-duration: 1s;
-        animation-iteration-count: infinite;
-        animation-fill-mode: both;
-        animation-timing-function: cubic-bezier(0, 0, 0, 0);
+    }
+    
+    #seletor{
+        position:absolute;
+        top: 0;
+        left: 300px;
+        z-index: 100;
+    }
+
+    .seletorAfter{
+        animation: seletor 0.125s linear 40;  
+    }
+    @keyframes seletor{
+        0%{
+            transform: rotate(0deg);
+        }100%{
+            transform: rotate(20deg);
+        }
+    }
+    @keyframes seletorAfter{
+        0%{
+            transform: rotate(0deg);
+        }100%{
+            transform: rotate(20deg);
+        }
     }
     @keyframes roleta{
         0%{
             transform: rotate(0deg);
         }
         100%{
-            transform: rotate(360deg);
+            transform: rotate(-360deg);
         }
     }
 </style>
@@ -21,9 +41,53 @@
 </head>
 <body>
     <div id="app">
-        <roleta></roleta>
+        <seletor id="seletor"></seletor>
+        <roleta id="roleta"></roleta>
     </div>
     <script src="{{elixir('js/app.js')}}"></script>
+    <script>
+    var numeroRandomico = Math.floor((Math.random() * 361) + 1);
+    console.log(numeroRandomico);
+    // setInterval(function(){
+    //     console.log(Math.floor(((Math.random() * 361) + 1) * -1));
+    // },1000)
+    var roleta = document.getElementById("roleta");
+    var seletor = document.getElementById("seletor");
+    roleta.addEventListener("click", function(){
+        seletor.style.animation = "seletor 0.125s linear 40";
+        roleta.style.animation = "roleta 1s linear 5";
+        setTimeout(function(){
+            seletor.style.animation = "seletorAfter 0.700s linear 8";
+            roleta.style.transition = "transform 5s ease-out";
+            roleta.style.transform = "rotate(-"+(360+numeroRandomico)+"deg)";
+        },5015)
+        setTimeout(function(){
+            retornarMensagem(numeroRandomico);
+        }, 11000);
+    })
+      
+    function retornarMensagem(numeroRandomico){
+        if(numeroRandomico > 0 && numeroRandomico <= 43){
+            alert("Parabéns! Você ganhou 10% de desconto");
+        }else if(numeroRandomico > 43 && numeroRandomico <= 87){
+            alert("Parabéns! Você ganhou 3% de desconto");
+        }else if(numeroRandomico > 87 && numeroRandomico <= 129){
+            alert("Parabéns! Você ganhou 5% de desconto");
+        }else if(numeroRandomico > 129 && numeroRandomico <= 176){
+            alert("Infelismente não foi dessa vez");
+        }else if(numeroRandomico > 176 && numeroRandomico <= 226){
+            alert("Parabéns! Você ganhou 5% de desconto");
+        }else if(numeroRandomico > 226 && numeroRandomico <= 269){
+            alert("Parabéns! Você ganhou 3% de desconto");
+        }else if(numeroRandomico > 269 && numeroRandomico <= 309){
+            alert("Parabéns! Você ganhou 10% de desconto");
+        }else{
+            alert("Infelismente não foi dessa vez");
+        }
+    }
     
+
+        
+    </script>
 </body>
 </html>
